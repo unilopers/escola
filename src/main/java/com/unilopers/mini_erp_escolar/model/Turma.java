96de9b4d423272f03f8edc956df84b74311f4ba2
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 12:50:00 -0300

    :sparkles: adicionar getters e setters ao model Turmas

--- src/main/java/com/unilopers/mini_erp_escolar/model/Turma.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Turma.java
@@ -14,4 +14,28 @@ public class Turma {
     @ManyToOne
     @JoinColumn(name = "professor_id")
     private Professor professor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
 }
