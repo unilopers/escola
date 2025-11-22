39475136a74674992e42886b04b808c8b3e35b34
Duda Sena <duda.sruiz@gmail.com>
2025-11-22 01:58:27 -0300

    :pencil2: fix: corrigir descrição do commit anterior (era repository, mas mensagem dizia controller)

--- src/main/java/com/unilopers/mini_erp_escolar/repository/DisciplinaRepository.java
+++ src/main/java/com/unilopers/mini_erp_escolar/repository/DisciplinaRepository.java
@@ -3,4 +3,6 @@ package com.unilopers.mini_erp_escolar.repository;
 import com.api.escola.model.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {}
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+
+}
