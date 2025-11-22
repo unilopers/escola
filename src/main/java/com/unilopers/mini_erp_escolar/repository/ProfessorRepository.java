f7ce0a9755f14e48a0a8ee0fefef56f3448d5e37
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 12:53:37 -0300

    :building_construction: criar repositório do Professor

---
+++ src/main/java/com/unilopers/mini_erp_escolar/repository/ProfessorRepository.java
@@ -0,0 +1,7 @@
+package com.unilopers.mini_erp_escolar.repository;
+
+import com.unilopers.mini_erp_escolar.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
