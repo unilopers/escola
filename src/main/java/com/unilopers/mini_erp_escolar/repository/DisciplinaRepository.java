d415f451894d8fb95064a2d5c633a81abab55f02
Duda Sena <duda.sruiz@gmail.com>
2025-11-22 01:52:22 -0300

    :sparkles: feat: implementar CRUD completo de Disciplina

---
+++ src/main/java/com/unilopers/mini_erp_escolar/repository/DisciplinaRepository.java
@@ -0,0 +1,6 @@
+package com.unilopers.mini_erp_escolar.repository;
+
+import com.api.escola.model.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {}
