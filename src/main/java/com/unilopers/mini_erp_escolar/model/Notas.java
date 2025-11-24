af95eccd00f3ce681d1e2660a9fb0cec2799b303
TomioNishijima <162518617+TomioNishijima@users.noreply.github.com>
2025-11-24 13:10:01 -0300

    :sparkles: feat: cria a entidade Nota JPA com mapeamento de FKs (Aluno e Disciplina)

---
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Notas.java
@@ -0,0 +1,40 @@
+package com.unilopers.mini_erp_escolar.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data; 
+import lombok.NoArgsConstructor;
+
+@Data 
+@NoArgsConstructor
+@Entity
+@Table(name = "notas") 
+public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idNota; 
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    @NotNull(message = "A nota é obrigatória.")
+    @DecimalMin(value = "0.00", message = "A nota mínima deve ser 0.00.")
+    @DecimalMax(value = "10.00", message = "A nota máxima deve ser 10.00.")
+    private BigDecimal nota; 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_aluno", nullable = false)
+    private Aluno aluno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_disciplina", nullable = false)
+    private Disciplina disciplina;
+
+    public Nota(BigDecimal nota, Aluno aluno, Disciplina disciplina) {
+        this.nota = nota;
+        this.aluno = aluno;
+        this.disciplina = disciplina;
+    }
+}
