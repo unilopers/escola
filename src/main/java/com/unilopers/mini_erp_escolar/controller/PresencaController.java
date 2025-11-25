adf9bd6bac46f1da14ae224d0ac114e14aca834c
LuizFelipe <LuizFelipe.Carmona@gmail.com>
2025-11-25 17:20:14 -0300

    :rocket: feat: criar PresencaController com rotas de registro e listagem

---
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/PresencaController.java
@@ -0,0 +1,34 @@
+package com.unilopers.mini_erp_escolar.controller;
+
+import com.unilopers.mini_erp_escolar.dto.PresencaRequestDTO;
+import com.unilopers.mini_erp_escolar.dto.PresencaResponseDTO;
+import com.unilopers.mini_erp_escolar.service.PresencaService;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/presencas")
+public class PresencaController {
+
+    private final PresencaService presencaService;
+
+    public PresencaController(PresencaService presencaService) {
+        this.presencaService = presencaService;
+    }
+
+    @PostMapping
+    public PresencaResponseDTO registrar(@RequestBody PresencaRequestDTO dto) {
+        return presencaService.registrar(dto);
+    }
+
+    @GetMapping("/aluno/{alunoId}")
+    public List<PresencaResponseDTO> listarPorAluno(@PathVariable Long alunoId) {
+        return presencaService.listarPorAluno(alunoId);
+    }
+
+    @GetMapping("/aula/{aulaId}")
+    public List<PresencaResponseDTO> listarPorAula(@PathVariable Long aulaId) {
+        return presencaService.listarPorAula(aulaId);
+    }
+}
