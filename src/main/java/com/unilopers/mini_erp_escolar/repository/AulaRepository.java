82b35d45db44dd1fca7be4ad900528ad4f6ce6b5
Duda Sena <duda.sruiz@gmail.com>
2025-11-25 01:43:54 -0300

    :sparkles: feat: adicionar AulaRepository

---
+++ src/main/java/com/unilopers/mini_erp_escolar/repository/AulaRepository.java
@@ -0,0 +1,7 @@
+package com.unilopers.mini_erp_escolar.repository;
+
+import com.unilopers.mini_erp_escolar.model.Aula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AulaRepository extends JpaRepository<Aula, Long> {
+}
