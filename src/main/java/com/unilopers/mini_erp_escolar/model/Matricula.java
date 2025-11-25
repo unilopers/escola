6813b476e0f2ac23afbf225d220b7ede4a72846a
MatheusHaverrothTarocco <matheustarocco@edu.unifil.br>
2025-11-24 22:54:05 -0300

    🐛Arrumando Bug Matricula.java

--- src/main/java/com/unilopers/mini_erp_escolar/model/Matricula.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Matricula.java
@@ -1,8 +1,12 @@
 package com.unilopers.mini_erp_escolar.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "matriculas",
         uniqueConstraints = @UniqueConstraint(columnNames = {"aluno_id", "turma_id"}))
 public class Matricula {
@@ -18,22 +22,4 @@ public class Matricula {
     @ManyToOne(optional = false)
     @JoinColumn(name = "turma_id")
     private Turma turma;
-
-    // getters e setters
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
-    public Aluno getAluno() {
-        return aluno;
-    }
-
-    public void setTurma(Turma turma) {
-        this.turma = turma;
-    }
-
-    public Turma getTurma() {
-        return turma;
-    }
 }
-
