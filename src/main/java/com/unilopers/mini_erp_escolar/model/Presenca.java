660ba4f5053c7a32a664567447bbfe4cf927455c
LuizFelipe <LuizFelipe.Carmona@gmail.com>
2025-11-25 17:13:45 -0300

    :fire: feat: alteração de getter/setter

--- src/main/java/com/unilopers/mini_erp_escolar/model/Presenca.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Presenca.java
@@ -1,30 +1,28 @@
 package com.unilopers.mini_erp_escolar.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "presencas", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"aluno_id", "aula_id"})
+        @UniqueConstraint(columnNames = {"id_aluno", "id_aula"})
 })
 public class Presenca {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_presenca")
     private Long id;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "aluno_id")
+    @JoinColumn(name = "id_aluno", nullable = false)
     private Aluno aluno;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "aula_id")
+    @JoinColumn(name = "id_aula", nullable = false)
     private Aula aula;
 
     @Column(nullable = false)
     private Boolean presente;
 
-    @Column(nullable = false)
-    private LocalDateTime dataRegistro = LocalDateTime.now();
-    //tentativa 3 de commit
+    // construtores, getters e setters
 }
